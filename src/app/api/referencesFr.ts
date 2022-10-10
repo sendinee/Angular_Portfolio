bf9a44319f3721c74dc05b733227d24f19bf5cd9
Zo <zoe.marday@outlook.com>
2022-10-10 15:30:58 +0200

    Initial commit

---
+++ src/app/api/referencesFr.ts
@@ -0,0 +1,24 @@
+export const referencesFr = [
+    {
+        personImg: "assets/images/unknown.jpg",
+        personFullname: 'Anonyme',
+        personPosition: "Directeur CEO",
+        personCompany: 'Eurivi Groupe',
+        message: "Une personne intéressante avec beaucoup d'enthousiasme.",
+    },
+    {
+        personImg: "assets/images/unknown.jpg",
+        personFullname: 'Anonyme',
+        personPosition: "Amie",
+        personCompany: 'Infirmière',
+        message: "C'est une personne avec un grand coeur qui est beaucoup curieuse, sérieuse dans son travaille.",
+    },
+    {
+        personImg: "assets/images/unknown.jpg",
+        personFullname: 'Anonyme',
+        personPosition: "Technicien de laboratoire",
+        personCompany: 'Scientech',
+        message: "Sendinee était une amie de classe avec qui j'ai garder contact. C'est une bosseuse et elle ne lache rien peut importe les difficultés.",
+    }
+];
+
